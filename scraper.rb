5aad2abd1a06e3d04586728b29ad290cbf3ae1e3
Tony Bowden <tony@tmtm.com>
2016-12-20 07:58:22 +0000

    Tidy up requires

--- scraper.rb
+++ scraper.rb
@@ -1,14 +1,12 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'scraperwiki'
+require 'date'
 require 'nokogiri'
 require 'open-uri'
-require 'cgi'
-require 'json'
-require 'date'
-
 require 'pry'
+require 'scraperwiki'
+
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
 
